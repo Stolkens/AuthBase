d45c3ce2ee5004a2fef546e073ad6e01b3bf805c
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-09-07 13:47:44 +0200

    Add profile endpoints

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
+const isLogged = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/user/no-permission');
+  }
+}
+
+router.get('/logged', isLogged, (req, res) => {
   res.render('logged');
 });
 
@@ -9,5 +17,12 @@ router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings');
+});
 
 module.exports = router;
